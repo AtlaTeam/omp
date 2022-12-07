f52ba7a2b24c5b5b0bcdb0f4f462c266b42c8c09
Patrick Buss <patrick@Patricks-MacBook-Pro.local>
2022-12-07 12:25:21 -0600

    Fleshing out child theme again

--- plugins/themes/atla-default/AtlaDefault.php
+++ plugins/themes/atla-default/AtlaDefault.php
@@ -19,7 +19,34 @@ class AtlaDefaultThemePlugin extends ThemePlugin {
 	 * @return null
 	 */
 	public function init() {
+		$this->setParent('defaultthemeplugin');
 
+//		// Override default styles for the "default" subtheme. Cookie Pro styling handled separately.
+//		$subtheme = $this->parent->getOption('bootstrapTheme');
+//		if ($subtheme == 'bootstrap3') {
+//			$this->addStyle('child-stylesheet', 'styles/atla.less');
+//			$this->modifyStyle('bootstrap', ['addLess' => ['styles/cookiepro.less']]);
+//		}
+//
+//		else {
+//			$this->modifyStyle("bootstrapTheme-{$subtheme}", ['addLess' => ['styles/cookiepro.less']]);
+//		}
+	}
+
+	/**
+	 * Tack on additional stylesheets for the given subtheme.
+	 *
+	 * @param string $subtheme
+	 *  The subtheme to modify.
+	 */
+	private function appendStyles($subtheme) {
+		// Styling for cookie banner.
+		$this->modifyStyle($subtheme, ['addLess' => ['styles/cookiepro.less']]);
+
+		// Styling for dev site banner.
+		if (Application::get()->getRequest()->getBaseUrl() !== 'https://books.atla.com') {
+			$this->modifyStyle($subtheme, ['addLess' => ['styles/dev-banner.less']]);
+		}
 	}
 
 	/**
